b6585a3cc3d137b563ac49872f4dd29b0dfe2c3b
Lyuruko <lyu01124899@gmail.com>
2024-03-11 09:07:01 -0400

    Initial commit

---
+++ Library/Get-OracleQueryResult/sample.ps1
@@ -0,0 +1,9 @@
+﻿$datasrce = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=XXXX)(PORT=XXXX)))(CONNECT_DATA=(SERVICE_NAME=XXXX)))"
+$username = "XXXX"
+$password = "XXXX"
+
+$query = @"
+SELECT * FROM XXXX
+"@
+
+Get-OracleQueryResult -DataSource $datasrce -UserName $username -Password $password -Query $query
