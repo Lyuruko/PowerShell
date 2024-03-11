e98d938a0778eecb365af35a5bf6f1a44552ea27
Lyuruko <lyu01124899@gmail.com>
2024-03-11 09:09:29 -0400

    Initial commit

--- Library/Get-QueryResult/sample.ps1
+++
@@ -1,9 +0,0 @@
-﻿$datasrce = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=XXXX.com)(PORT=XXXX)))(CONNECT_DATA=(SERVICE_NAME=XXXX)))"
-$username = "XXXX"
-$password = "XXXX"
-
-$query = @"
-SELECT * FROM XXXX
-"@
-
-Get-QueryResult -DataSource $datasrce -UserName $username -Password $password -Query $query
